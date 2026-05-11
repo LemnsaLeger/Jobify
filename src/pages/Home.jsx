6430c054580664fc2ef7808fb49b99da32c8b22d
LemnsaLeger <lemnsadigitalservices@gmail.com>
2026-05-11 23:43:48 +0100

    Modified: src/pages/Home.jsx, desc: Added the Dashboard component to home page.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,6 +12,9 @@ import {
 import { useState } from "react";
 import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
+import Dashboard from "../components/dashboard";
+
+import "../../src/index.css";
 
 const navItems = [
   { name: "Dashboard", icon: LayoutDashboard, href: "#" },
@@ -26,15 +29,22 @@ export default function Home({children}) {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
     return (
       <div className="min-h-screen bg-gray-50 flex">
-        <Sidebar isSidebarOpen={isSidebarOpen} navItems={navItems}/>
+        <Sidebar isSidebarOpen={isSidebarOpen} navItems={navItems} />
 
         {/* --- Main Content Area --- */}
-        <div className="flex-1 flex flex-col min-w-0">
-          <Navbar setIsSidebarOpen={setIsSidebarOpen} isSidebarOpen={isSidebarOpen} />
+        <div className="h-20 flex-1 flex flex-col w-auto justify-between lg:ml-5 sm:w-full md:w-full bg-amber-700">
+          <Navbar
+            setIsSidebarOpen={
+              () => setIsSidebarOpen(!isSidebarOpen)
+            }
+            isSidebarOpen={isSidebarOpen}
+          />
         </div>
 
         {/* Dashboard Content */}
-        <main className="p-4 lg:p-8 overflow-y-auto">{children}</main>
+        <main className="p-4 lg:p-8 overflow-y-auto">
+          <Dashboard />
+        </main>
       </div>
     );
 }
